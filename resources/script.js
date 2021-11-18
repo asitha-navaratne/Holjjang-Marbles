0341627125b51db2908672413b69062257b19e53
Asitha Navaratne <asithan476@gmail.com>
2021-11-18 15:52:04 +0530

    Bug in logic selecting bet value

--- resources/script.js
+++ resources/script.js
@@ -8,43 +8,77 @@ const resetBtn = document.getElementById("reset-btn");
 const player = {
   score: 10,
   bet: 0,
-  call: "",
   scoreDisplay: document.getElementById("player-score"),
   betDisplay: document.getElementById("player-bet"),
+  setDisplayValues: setDisplayValues,
 };
 
 const cpu = {
   score: 10,
   bet: 0,
-  call: "",
   scoreDisplay: document.getElementById("cpu-score"),
   betDisplay: document.getElementById("cpu-bet"),
+  setDisplayValues: setDisplayValues,
 };
 
-const players = [player, cpu];
+let call = "";
+const callList = ["odd", "even"];
 
 let isPlayersTurn = true;
 let isBetValid = false;
-let isPlayerWinner = false;
-
-const callList = ["odd", "even"];
 
 const generateCpuBet = (score) =>
   score > 5
     ? Math.round(Math.random() * 4) + 1
     : Math.round(Math.random() * (score - 1)) + 1;
 
+const determineWinner = function (call, player1, player2) {
+  if (
+    (call === "even" && player2.bet % 2 === 0) ||
+    (call === "odd" && player2.bet % 2 !== 0)
+  ) {
+    player1.score += player2.bet;
+    player2.score -= player2.bet;
+  } else {
+    player1.score -= player2.bet;
+    player2.score += player2.bet;
+  }
+  player1.setDisplayValues();
+  player2.setDisplayValues();
+};
+
+function setDisplayValues() {
+  this.scoreDisplay.innerText = this.score;
+  this.betDisplay.innerText = this.bet;
+}
+
 selectBet.addEventListener("input", function () {
-  if (parseInt(selectBet.value) > player.score) {
+  if (parseInt(this.value) > player.score) {
     callDisplay.innerText = "Not enough money to make this bet!";
     isBetValid = false;
   } else {
     isBetValid = true;
+    callDisplay.innerText = "Make your call";
+    player.bet = parseInt(selectBet.value);
+    cpu.bet = generateCpuBet(cpu.score);
   }
 });
 
 submitBtn.addEventListener("click", function () {
   if (isBetValid) {
+    if (isPlayersTurn) {
+      call = selectCall.value;
+      callDisplay.innerText = `Player called ${call}!`;
+      determineWinner(call, player, cpu);
+      isPlayersTurn = false;
+      turnDisplay.innerText = "Opponent's turn";
+    } else {
+      call = callList[Math.round(Math.random())];
+      callDisplay.innerText = `CPU called ${call}!`;
+      determineWinner(call, cpu, player);
+      isPlayersTurn = true;
+      turnDisplay.innerText = "Your turn";
+    }
   } else {
     callDisplay.innerText = "Please make a valid bet!";
   }
@@ -53,19 +87,17 @@ submitBtn.addEventListener("click", function () {
 resetBtn.addEventListener("click", function () {
   isPlayersTurn = true;
   isBetValid = false;
-  isPlayerWinner = false;
 
-  players.forEach((player) => {
-    player.score = 10;
-    player.bet = 0;
-    player.call = "";
-    player.scoreDisplay.innerText = "10";
-    player.betDisplay.innerText = "0";
-  });
+  for (let p of [player, cpu]) {
+    p.score = 10;
+    p.bet = 0;
+    p.scoreDisplay.innerText = "10";
+    p.betDisplay.innerText = "0";
+  }
 
   turnDisplay.innerText = "Your turn!";
   callDisplay.innerText = "Place your bet";
 
-  selectBet.value = "";
-  selectCall.value = "";
+  selectBet.value = "1";
+  selectCall.value = "odd";
 });
